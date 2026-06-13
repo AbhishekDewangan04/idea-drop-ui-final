8f28aff6e7b71447a24af4b1ebae7475c5874407
Dewangan Abhishek <abhishekdewangan8910@gmail.com>
2026-06-13 22:36:55 +0530

    Fix production environment URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,41 +3,59 @@ import { getStoredAccessToken, setStoredAccessToken } from "./authToken";
 import { refreshAccessToken } from "@/api/auth";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_PRODUCTION_API_URL}/api`,
+  baseURL:
+    import.meta.env.VITE_PRODUCTION_API_URL ||
+    "http://localhost:8000/api",
+
   headers: {
     "Content-Type": "application/json",
   },
+
   withCredentials: true,
 });
 
-//Attach Token on refresh
+// Attach Token
 api.interceptors.request.use((config) => {
   const token = getStoredAccessToken();
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
-//Refresh Token after expire
+// Refresh Token
 api.interceptors.response.use(
   (res) => res,
+
   async (error) => {
     const requestConfig = error.config;
+
     if (
       error.response?.status === 401 &&
       !requestConfig.url.includes("/auth/refresh")
     ) {
       try {
-        const { accessToken: newToken } = await refreshAccessToken();
+        const { accessToken: newToken } =
+          await refreshAccessToken();
+
         setStoredAccessToken(newToken);
-        requestConfig.headers.Authorization = `Bearer ${newToken}`;
+
+        requestConfig.headers.Authorization =
+          `Bearer ${newToken}`;
+
         return api(requestConfig);
-      } catch (error) {
-        console.error("Refreshing Access Token failed, ", error);
+      } catch (err) {
+        console.error(
+          "Refreshing Access Token failed",
+          err
+        );
       }
     }
+
     return Promise.reject(error);
   }
 );
+
 export default api;
